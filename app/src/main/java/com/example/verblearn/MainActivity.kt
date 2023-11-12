c4c65b8c68aad36fb19b09ad33208cb01be8d782
Jorge Toribio <jorgeluistoribio14@gmail.com>
2023-11-12 01:18:41 -0400

    first commit

---
+++ app/src/main/java/com/example/verblearn/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.verblearn
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.Tab
+import androidx.compose.ui.semantics.Role.Companion.Tab
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.verblearn.ui.theme.VerbLearnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VerbLearnTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VerbLearnTheme {
+        Greeting("Android")
+    }
+}
+
---
+++ app/src/main/java/com/example/verblearn/ui/theme/verb/SplashScreen.kt
@@ -0,0 +1,79 @@
+package com.example.verblearn.ui.theme.verb
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.verblearn.R
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SplashScreen()
+{
+    val color = Color(0xFF65D0F2)
+    val colorBlanco = Color(0xFFFFFFFF)
+
+    val text = AnnotatedString.Builder("VerbLearn")
+    .apply {
+        addStyle(
+            style = SpanStyle(color),
+            start = 0,
+            end = 4
+        )
+        addStyle(
+            style = SpanStyle(colorBlanco),
+            start = 4,
+            end = 9
+        )
+    }
+    .toAnnotatedString()
+
+    Box(
+        modifier = Modifier
+            .width(430.dp)
+            .height(932.dp)
+            .background(color = Color(0xFF191D2B)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 0.dp)
+        ) {
+            Text(
+                text = text,
+                fontSize = 48.sp,
+                fontWeight = FontWeight(400),
+            )
+            Image(
+                painter = painterResource(id = R.drawable._223),
+                contentDescription = "Descripción de la imagen",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(300.dp)
+
+            )
+        }
+    }
+
+
+
+
+}
