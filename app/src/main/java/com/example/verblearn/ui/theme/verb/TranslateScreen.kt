a72ad06e73a92978cd99872518fc7068f65ade94
Jorge Toribio <jorgeluistoribio14@gmail.com>
2023-11-24 00:42:59 -0400

    update-switch

--- app/src/main/java/com/example/verblearn/ui/theme/verb/TranslateScreen.kt
+++ app/src/main/java/com/example/verblearn/ui/theme/verb/TranslateScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.StarBorder
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -24,16 +25,20 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Switch
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -53,6 +58,7 @@ fun TranslateScreen(idVerb: Int, viewModel: VerbViewModel = hiltViewModel()) {
     val colorCard = Color(0xFFFFFFFF)
     val ColorCardDefinition = Color(0x57D9DDEA)
     var translateOn by mutableStateOf(false)
+    var checked by remember { mutableStateOf(true) }
 
     DisposableEffect(Unit) {
         viewModel.getVerbById(idVerb)
@@ -212,39 +218,29 @@ fun TranslateScreen(idVerb: Int, viewModel: VerbViewModel = hiltViewModel()) {
 
                 )
                 {
-                    val color = Color(0xFF191D2B)
-                    Button(
-                        onClick = { translateOn = !translateOn},
-                        Modifier
-                            .width(182.dp)
-                            .height(50.dp),
-                        shape = RoundedCornerShape(size = 10.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(0xFF191D2B),
-                            contentColor = Color(0xFF191D2B)
-                        )
+                    Text(
+                        text = "Translate",
+                        fontSize = 13.sp
                     )
-                    {
-                        if(translateOn){
-                            Text(
-                                text = "Translate (OFF)",
-                                style = TextStyle(
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight(400),
-                                    color = Color(0xFFFFFFFF),
+                    Switch(
+                        checked = translateOn,
+                        onCheckedChange = {
+                            translateOn = it
+                        },
+                        thumbContent = if (translateOn) {
+                            {
+                                Icon(
+                                    imageVector = Icons.Filled.Check,
+                                    contentDescription = null,
+                                    modifier = Modifier.size(SwitchDefaults.IconSize),
                                 )
-                            )
-                        }else{
-                            Text(
-                                text = "Translate (ON)",
-                                style = TextStyle(
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight(400),
-                                    color = Color(0xFFFFFFFF),
-                                )
-                            )
+                            }
+                        } else {
+                            null
                         }
-                    }
+
+                    )
+
 
                 }
                 Row {
